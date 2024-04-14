3e5ac19b2e2a57b07957f9ebbffc2ec0498b3985
praveencloudangles <112871831+praveencloudangles@users.noreply.github.com>
2024-04-14 19:39:38 +0530

    Update datavisualization.py

--- datavisualization.py
+++ datavisualization.py
@@ -1,7 +1,9 @@
+print("data visulization-------")
 from data_cleaning import data_cleaning
-from data_analysis import data_analysis
+from feature_engineering import feature_eng
 import pandas as pd
 import plotly.express as px
+from IPython.display import Image
 import warnings
 warnings.filterwarnings("ignore")
 import numpy as np
@@ -12,72 +14,63 @@ import plotly.io as pio
 import plotly.graph_objects as go
 import io
 from PIL import Image
+a =[]
+def data_visualization():
+    dataset = data_cleaning()
 
-def data_vis():
-    data = data_cleaning()
-    print(data)
-
-    df=data_analysis()
+    data = feature_eng()
+    print("dfghjk--------------------------", data)
 
 
-    top_products = df['type_of_meal_plan'].value_counts().head(10).index.tolist()
-    filtered_df = df[df['type_of_meal_plan'].isin(top_products)]
+    car_df = dataset['type'].value_counts().rename_axis('type').reset_index(name='count')
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=car_df['type'],
+        y=car_df['count']
+    ))
+    fig.update_layout(template="plotly_dark", title="Distribution - Required Payment Type")
+    fig.write_image("payment_type.jpg")
 
-    product_counts = filtered_df['type_of_meal_plan'].value_counts()
+    #------------------------------------------------------------------------------
+
+
+    car = data['isFraud'].value_counts().rename_axis('isFraud').reset_index(name='count')
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=car['isFraud'],
+        y=car['count']
+    ))
+    fig.update_layout(template="plotly_dark", title="Distribution - Is Fraud")
+    fig.write_image("Is_Fraud.jpg")
+
+    #---------------------------------------------------------------------------------------
+
+    top_products = dataset['type'].value_counts().head(10).index.tolist()
+    filtered_df = dataset[dataset['type'].isin(top_products)]
+
+    product_counts = filtered_df['type'].value_counts()
 
     fig = go.Figure(data=[go.Pie(labels=product_counts.index, values=product_counts.values)])
     
     fig.update_layout(
         template='plotly_dark',
-        title='Top 10 Products by Occurrences in Transactions'
+        title='Payment Types'
     )
 
     fig.write_image("pie_chart.jpg")  
-
-
-    dist_df = df['type_of_meal_plan'].value_counts().rename_axis('type_of_meal_plan').reset_index(name='count')
-    fig = go.Figure()
-    fig.add_trace(go.Bar(
-        x=dist_df['type_of_meal_plan'],
-        y=dist_df['count']
-    ))
-    fig.update_layout(template="plotly_dark", title="Distribution - Type of meal plan")
-    fig.write_image("meal_plan_distribution.jpg")
-
-#-----------------------------
-
-    car_df = df['required_car_parking_space'].value_counts().rename_axis('required_car_parking_space').reset_index(name='count')
-    fig = go.Figure()
-    fig.add_trace(go.Bar(
-        x=car_df['required_car_parking_space'],
-        y=car_df['count']
-    ))
-    fig.update_layout(template="plotly_dark", title="Distribution - Required Car Parking Space")
-    fig.write_image("Required_Car_Parking_Space.jpg")
-
-#-------------------------------------------
-    
-    market_df = df['market_segment_type'].value_counts().rename_axis('market_segment_type').reset_index(name='count')
-    fig = go.Figure()
-    fig.add_trace(go.Bar(
-        x=market_df['market_segment_type'],
-        y=market_df['count']
-    ))
-    fig.update_layout(template="plotly_dark", title="Distribution - Market Segment Type")
-    fig.write_image("Market_Segment_Type.jpg")
-
         
-    columns_to_remove = ['arrival_year', 'arrival_month', 'arrival_date', 'no_of_week_nights', 'no_of_weekend_nights', 'no_of_previous_cancellations']
-    df=data.drop(columns=columns_to_remove,axis=1)
+    #---------------------------------------------------------------------------------------
+    
+    df=dataset.drop("isFraud",axis=1)
     y=df.corr().columns.tolist()
     z=df.corr().values.tolist()
     z_text = np.around(z, decimals=4) # Only show rounded value (full value on hover)
     fig = ff.create_annotated_heatmap(z,x=y,y=y,annotation_text=z_text,colorscale=px.colors.sequential.Cividis_r,showscale=True)
-    fig.update_layout(template='plotly_dark', width=1000, height=700)
+    fig.update_layout(template='plotly_dark')
     # fig.show()
-    fig.write_image("img.jpg")
+    fig.write_image("heat_map.jpg")
+    # a.append(fig)
+    
+    return dataset
 
-
-    return data
-
-data_vis()
+data_visualization()
